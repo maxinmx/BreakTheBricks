aaea3bff366a7ca9ecaf748bcaacc85e12aec155
maxin <mx1759808857@163.com>
2018-04-07 11:19:19 +0800

    打砖块游戏

---
+++ BreakTheBricksProject/Assets/Scripts/Shoot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+    public float speed = 5;
+    public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject b = GameObject.Instantiate(bullet, transform.position, transform.rotation);
+            Rigidbody rgd = b.GetComponent<Rigidbody>();
+            rgd.velocity = transform.forward * speed;
+
+        }
+    }
+}
